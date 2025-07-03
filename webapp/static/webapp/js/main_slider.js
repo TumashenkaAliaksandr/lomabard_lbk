60ffcce5e55817a2609758dcdc7464b4a16ad98b
Admin <tumashenkaaliaksandr@gmail.com>
2025-07-03 21:53:13 +0300

    added new styles for buttons, and fixing js fpr slides, refactoring and adaptation main slider template

--- webapp/static/webapp/js/main_slider.js
+++ webapp/static/webapp/js/main_slider.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const slidesCount = 3; // Количество слайдов
+    const slidesCount = document.querySelectorAll('.hero-slider input[type="radio"]').length;
     let currentSlide = 1;
-    const intervalTime = 5000; // Интервал переключения в миллисекундах (5 секунд)
+    const intervalTime = 5000; // 5 секунд
 
     setInterval(() => {
         currentSlide++;
         if (currentSlide > slidesCount) {
             currentSlide = 1;
         }
-        // Активируем соответствующую радио-кнопку
         const radio = document.getElementById(`slide${currentSlide}`);
         if (radio) {
             radio.checked = true;
